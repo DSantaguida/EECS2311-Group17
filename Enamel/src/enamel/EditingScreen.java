ad08cb3bdfe7acb52c05e991600cecd7a4906369
Yoni Fihrer <yoni@fihrer.com>
2018-03-28 14:29:38 -0400

    Added Graphcanvas to editingscreen
    This was to allow for more flexibility of where to put the next prev and
    curr nodes

--- Enamel/src/enamel/EditingScreen.java
+++ Enamel/src/enamel/EditingScreen.java
@@ -6,6 +6,7 @@ import javax.swing.plaf.basic.BasicArrowButton;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.RoundRectangle2D;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -74,6 +75,11 @@ public class EditingScreen implements ActionListener {
 	private JPanel buttonCard;
 	private GraphCanvas graphCanvas;
 	protected boolean testActionListenerActive = true;
+	private JLabel lblPreviousNodes;
+	private JComboBox<Node> comboBoxPrevNodes;
+	private JButton btnNode;
+	private JLabel lblNextNodes;
+	private JComboBox<Node> comboBoxNextNodes;
 
 	// int[] indvCell = new int[boxCount];
 
@@ -183,10 +189,7 @@ public class EditingScreen implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		graphCanvas = new GraphCanvas(scenario, scenario.getHead());
-		graphCanvas.setBounds(0, 0, 550, 525);
-		frame.getContentPane().add(graphCanvas);
-		graphCanvas.setVisible(true);
+		
 
 		// Initialize Main JPanel
 		panel = new JPanel();
@@ -652,25 +655,15 @@ public class EditingScreen implements ActionListener {
 		});
 		panel_3.add(btnMainMenu);
 
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(481, 606, 152, 109);
-		panel.add(panel_6);
-		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.Y_AXIS));
+		lblPreviousNodes = new JLabel("Previous Nodes:");
 
-		JLabel lblPreviousNodes = new JLabel("Previous Nodes:");
-		panel_6.add(lblPreviousNodes);
+		comboBoxPrevNodes = new JComboBox<>();
 
-		JComboBox<Node> comboBoxPrevNodes = new JComboBox<>();
-		panel_6.add(comboBoxPrevNodes);
+		btnNode = new JButton("Current Node");
 
-		JButton btnNode = new JButton("Current Node");
-		panel_6.add(btnNode);
+		lblNextNodes = new JLabel("Next Nodes:");
 
-		JLabel lblNextNodes = new JLabel("Next Nodes:");
-		panel_6.add(lblNextNodes);
-
-		JComboBox<Node> comboBoxNextNodes = new JComboBox<>();
-		panel_6.add(comboBoxNextNodes);
+		comboBoxNextNodes = new JComboBox<>();
 
 		btnNode.setVisible(true);
 		btnNode.addActionListener(new ActionListener() {
@@ -686,72 +679,14 @@ public class EditingScreen implements ActionListener {
 			}
 		});
 
-		for (Node node : this.scenario.getPrevNodes(this.currentNode)) {
-			comboBoxPrevNodes.addItem(node);
-		}
-		for (Node node : this.scenario.getNextNodes(this.currentNode)) {
-			comboBoxNextNodes.addItem(node);
-		}
-
-		comboBoxNextNodes.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (testActionListenerActive) {
-					testActionListenerActive = false;
-					currentNode = (Node) comboBoxNextNodes.getSelectedItem();
-					CardLayout cl = (CardLayout) (optionCard.getLayout());
-					cl.show(optionCard, "Do Nothing");
-					lblCurrentButton.setText("Node Selected");
-					graphCanvas.setNode(currentNode);
-					graphCanvas.repaint();
-					comboBoxNextNodes.removeAllItems();
-					for (Node node : scenario.getNextNodes(currentNode)) {
-						comboBoxNextNodes.addItem(node);
-					}
-					comboBoxPrevNodes.removeAllItems();
-					for (Node node : scenario.getPrevNodes(currentNode)) {
-						comboBoxPrevNodes.addItem(node);
-					}
-					loadNodeEvents();
-					testActionListenerActive = true;
-				}
-
-			}
-		});
-
-		comboBoxPrevNodes.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (testActionListenerActive) {
-					testActionListenerActive = false;
-					currentNode = (Node) comboBoxPrevNodes.getSelectedItem();
-					CardLayout cl = (CardLayout) (optionCard.getLayout());
-					cl.show(optionCard, "Do Nothing");
-					lblCurrentButton.setText("Node Selected");
-					graphCanvas.setNode(currentNode);
-					graphCanvas.repaint();
-					comboBoxNextNodes.removeAllItems();
-					if (scenario.hasNextNodes(currentNode)) {
-						for (Node node : scenario.getNextNodes(currentNode)) {
-							comboBoxNextNodes.addItem(node);
-						}
-					}
-					comboBoxPrevNodes.removeAllItems();
-					for (Node node : scenario.getPrevNodes(currentNode)) {
-						comboBoxPrevNodes.addItem(node);
-					}
-					loadNodeEvents();
-					testActionListenerActive = true;
-				}
-			}
-		});
+		
 		// loadNodeEvents();
 	}
 
 	private void initEventMods() {
 		JPanel panel_5 = new JPanel();
 		eventPanel.add(panel_5);
-		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_5.setLayout(new FlowLayout(FlowLayout.CENTER, 3, 5));
 
 		JLabel lblEventEditor = new JLabel("Event Editor:");
 		lblEventEditor.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -1087,6 +1022,73 @@ public class EditingScreen implements ActionListener {
 			cellBox.addItem(i);
 		}
 		panel_2.add(cellBox);
+		
+		graphCanvas = new GraphCanvas(scenario, scenario.getHead());
+		for (Node node : this.scenario.getPrevNodes(this.currentNode)) {
+			comboBoxPrevNodes.addItem(node);
+		}
+		for (Node node : this.scenario.getNextNodes(this.currentNode)) {
+			comboBoxNextNodes.addItem(node);
+		}
+
+		comboBoxNextNodes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (testActionListenerActive) {
+					testActionListenerActive = false;
+					currentNode = (Node) comboBoxNextNodes.getSelectedItem();
+					CardLayout cl = (CardLayout) (optionCard.getLayout());
+					cl.show(optionCard, "Do Nothing");
+					lblCurrentButton.setText("Node Selected");
+					graphCanvas.setNode(currentNode);
+					graphCanvas.repaint();
+					comboBoxNextNodes.removeAllItems();
+					for (Node node : scenario.getNextNodes(currentNode)) {
+						comboBoxNextNodes.addItem(node);
+					}
+					comboBoxPrevNodes.removeAllItems();
+					for (Node node : scenario.getPrevNodes(currentNode)) {
+						comboBoxPrevNodes.addItem(node);
+					}
+					loadNodeEvents();
+					testActionListenerActive = true;
+				}
+
+			}
+		});
+
+		comboBoxPrevNodes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (testActionListenerActive) {
+					testActionListenerActive = false;
+					currentNode = (Node) comboBoxPrevNodes.getSelectedItem();
+					CardLayout cl = (CardLayout) (optionCard.getLayout());
+					cl.show(optionCard, "Do Nothing");
+					lblCurrentButton.setText("Node Selected");
+					graphCanvas.setNode(currentNode);
+					graphCanvas.repaint();
+					comboBoxNextNodes.removeAllItems();
+					if (scenario.hasNextNodes(currentNode)) {
+						for (Node node : scenario.getNextNodes(currentNode)) {
+							comboBoxNextNodes.addItem(node);
+						}
+					}
+					comboBoxPrevNodes.removeAllItems();
+					for (Node node : scenario.getPrevNodes(currentNode)) {
+						comboBoxPrevNodes.addItem(node);
+					}
+					loadNodeEvents();
+					testActionListenerActive = true;
+				}
+			}
+		});
+		graphCanvas.setBounds(0, 0, 550, 525);
+		frame.getContentPane().add(graphCanvas);
+		graphCanvas.setVisible(true);
+		graphCanvas.repaint();
+		graphCanvas.revalidate();
+		
 	}
 
 	private void setPinVisible(boolean b) {
@@ -1420,4 +1422,140 @@ public class EditingScreen implements ActionListener {
 		eventPanel.revalidate();
 		eventPanel.repaint();
 }
+	@SuppressWarnings("serial")
+	public class GraphCanvas extends JPanel {
+		public int startingX;
+		public int startingY;
+		public int width;
+		public int height;
+		public Node n;
+		public Scenario s;
+		
+		
+		/**
+		 * Create the application.
+		 * @wbp.parser.entryPoint
+		 */
+		public GraphCanvas(Scenario s, Node n) {
+			super();
+			this.n = n;
+			this.s = s;
+			initialize();
+			
+		}
+
+		/**
+		 * Initialize the contents of the frame.
+		 * @wbp.parser.entryPoint
+		 */ 
+		private void initialize() {
+//			graphCanvas.setBounds(0, 0, 550, 525);
+			
+			this.startingX = 50;
+			this.startingY = 23;
+			this.width = 500;
+			this.height = 484;
+			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			this.add(comboBoxPrevNodes);
+			this.add(btnNode);
+			this.add(comboBoxNextNodes);
+			this.add(lblNextNodes);
+			this.add(lblPreviousNodes);
+			
+		}
+		
+		public void update() {
+			repaint();
+		}
+		
+		public void setNode(Node node) {
+			this.n = node;
+		}
+	    /**
+	     * @wbp.parser.entryPoint
+	     */
+	    public void paint(Graphics g) {
+	    	super.paintComponent(g);
+	        Graphics2D graphics2 = (Graphics2D) g;
+	        graphics2.setStroke(new BasicStroke(2));
+	        Rectangle Rectangle = new Rectangle (this.startingX, this.startingY, this.width, this.height);
+	        graphics2.draw(Rectangle);
+	        
+	        int width = 200;
+	        int height = 75;
+	        
+	        int nodeX = this.width / 2 + this.startingX - width / 2;
+	        int nodeY = this.height / 2 + this.startingY- height / 2;
+	        
+	        graphics2.drawString("Current Node", nodeX + 3, this.height/2);
+	        graphics2.drawString(n.getResponses()[0].substring(0, Math.min(n.getResponses()[0].length(), 26)) + "...",
+	        		nodeX + 18, this.height/2+30);
+
+	        RoundRectangle2D roundedRectangle = new RoundRectangle2D.Float(nodeX, nodeY, width, height, 10, 10);
+	        graphics2.draw(roundedRectangle);
+	        
+	        int lineEnd = this.height/2+this.startingY-height/2-100;
+	        graphics2.drawLine(this.width/2 +this.startingX, this.height/2+this.startingY-height/2, this.width/2 + this.startingX, lineEnd);
+	        
+	        roundedRectangle = new RoundRectangle2D.Float(nodeX, lineEnd-height, width, height, 10, 10);
+	        graphics2.draw(roundedRectangle);
+	        
+	        
+	        if (!n.equals(this.s.getHead())) {
+	        	graphics2.drawString("First node of the Scenario", nodeX + 3, lineEnd - height+15);
+	            graphics2.drawString(s.getHead().getResponses()[0].substring(0, Math.min(s.getHead().getResponses()[0].length(), 26)) + "...",
+	            		nodeX + 20, lineEnd - height/2);
+	            
+	        } else {
+	        	Font oldFont = graphics2.getFont();
+	        	Font newFont = new Font(oldFont.getName(), Font.BOLD, (int) (oldFont.getSize() * 1.2));
+//	        	Font newFont = oldFont.deriveFont(oldFont.getSize() * 1.3F);
+	        	graphics2.setFont(newFont);
+	        	graphics2.drawString( "This is the beggining", nodeX + 19, lineEnd - height +30);
+	        	graphics2.drawString("of the Scenario", nodeX + 40, lineEnd - height +50);
+	        	g.setFont(oldFont);
+	        }
+	        
+	        lineEnd = this.height/2+this.startingY+height/2+100;
+	      
+	        roundedRectangle = new RoundRectangle2D.Float(this.width/2+this.startingX-width+10, lineEnd, width, height, 10, 10);
+	        
+	        roundedRectangle = new RoundRectangle2D.Float(this.width/2+this.startingX+10, lineEnd, width, height, 10, 10);
+	        this.createRectangles(graphics2, nodeX, nodeY + height, width, lineEnd, 10,  height);
+	    }
+	    
+	    
+	    public void createRectangles(Graphics2D g, int firstX, int bottom, int firstWidth, int y, int space, int height) {
+	    	if (!s.hasNextNodes(n)) {
+	    		return;
+	    	}
+	    	Node[] nextNodes = s.getNextNodes(n);
+	    	int num = nextNodes.length;
+	    	int spaces = (num + 1) * space;
+	    	int width = (this.width - spaces) / Math.max(num, 1);
+	    	int rect = space + width;
+	    	int x = 0;
+	    	int dline = firstWidth/(num+1);
+	    	for (int i = 0; i < num; i++) {
+	    		x = space + this.startingX + rect * i;
+	    		drawRectangle(g, firstX, bottom, y, height, nextNodes[i], width, x, dline, i);
+	    	}
+	    }
+
+		private void drawRectangle(Graphics2D g, int firstX, int bottom, int y, int height, Node node, int width,
+				int x, int dline, int i) {
+			
+			g.draw(new RoundRectangle2D.Float(x, y, width, height, 10, 10));
+			g.drawString("Node " + (i+1), x+3, y+14);
+			String response = node.getResponses()[0];
+			int length = Math.min(response.length(), width*9/55-74/11);
+			String addition = "";
+			if (length != response.length())
+				addition = "...";
+			g.drawString(response.substring(0, length) + addition,
+					x + 18, y+44);
+			g.drawLine(firstX + dline * (i + 1), bottom, x + width / 2, y);
+		}
+
+	}
 }
